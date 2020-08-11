d1ab602d45cd8a6be8641dcef01847d67cf72308
Andrey Simakov <admin@web-say.com>
2020-08-11 19:03:05 +0300

    Add

--- src/Controller/WsCrmController.php
+++ src/Controller/WsCrmController.php
@@ -26,25 +26,25 @@ class WsCrmController {
    */
 
   public function panelshome() {
-    
+
     $element = [];
-    
+
     $config = \Drupal::config('wscrm.settings');
-    
+
     $element['#title'] = $config->get('wscrm.page_title') ? $config->get('wscrm.page_title') : Html::escape(t('WS CRM Panels'));
-    
+
     $element[]= array(
               '#cache' => ['max-age' => 0,],
               '#markup' => $config->get('wscrm.source_text')
             );
-            
+
     /** PROJECT **/
     $count_project = wsrcm_project_count();
-    
+
     $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
     $_url = Url::fromRoute('wscrm.projectcreate', [], ['language' => $language]);
     $userCurrent = \Drupal::currentUser();
-    
+
     if ($_url->access($userCurrent)) {
       $url_create_project= Url::fromRoute('wscrm.projectcreate');
       $url_create_project_options = array( 'attributes' => array( 'class' => array('btn','btn-success' ), ),);
@@ -54,7 +54,7 @@ class WsCrmController {
     } else{
       $link_create_project_link = '';
     }
-    
+
     $url_list_project = Url::fromRoute('wscrm.projectlist');
     $url_list_project_options = array( 'attributes' => array( 'class' => array('btn','btn-default' ), ),);
     $url_list_project->setOptions($url_list_project_options);
@@ -62,7 +62,7 @@ class WsCrmController {
 
     /** TASK **/
     $count_task = wsrcm_task_count();
-    
+
     $_url = Url::fromRoute('wscrm.taskcreate', [], ['language' => $language]);
     $userCurrent = \Drupal::currentUser();
     if ($_url->access($userCurrent)) {
@@ -81,11 +81,11 @@ class WsCrmController {
 
     /** Company **/
     $count_сompany = wsrcm_company_count();
-    
+
     $_url = Url::fromRoute('wscrm.companycreate', [], ['language' => $language]);
     $userCurrent = \Drupal::currentUser();
     if ($_url->access($userCurrent)) {
-      
+
       $url_create_сompany = Url::fromRoute('wscrm.companycreate');
       $url_create_сompany_options = array( 'attributes' => array( 'class' => array('btn','btn-success' ), ),);
       $url_create_сompany->setOptions($url_create_task_options);
@@ -107,6 +107,9 @@ class WsCrmController {
                       <h3>'.t('Companyes').' ('.$count_сompany.')</h3>
                       <p>'.$link_list_сompany_link.' '.$link_create_сompany_link.'</p>
                     </div>
+                    <ul>
+                      <li> -</li>
+                    </ul>
                   </div>
                 </div>
                   <div class="col-sm-6 col-md-4 col-xs-12 col-lg-4">
@@ -132,7 +135,7 @@ class WsCrmController {
               '#cache' => ['max-age' => 0,],
               '#markup' => $content
             );
-            
+
     return $element;
 
   }
